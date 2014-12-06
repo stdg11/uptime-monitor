a7a21d84107d308d2641fca999a969a305acf75c
Daniel Grammatica <daniel.grammatica@bourne-grammar.lincs.sch.uk>
2014-12-06 16:54:13 +0000

    Added config values

--- TODO.md
+++ TODO.md
@@ -3,9 +3,11 @@ TODO
 
  * Add timer
  * Different Warnings on Status Code e.g. SSL error
+ * Add support for Servers with hardened SSL
  * <del>Refresh WebPage every minute</del>
  * <del>Log rotation</del>
  * <del>Loop through hosts in config file</del>
  * <del>Only send the one email when host goes down</del>
  * <del>keep status of site in one tuple/dict [{site, isup},{remote.bo...,True}]</del>
 
+ * onError email restored
--- uptime_monitor.py
+++ uptime_monitor.py
@@ -6,8 +6,11 @@ logging.info('Started')
 #Load config.ini
 parser = configparser.ConfigParser() 
 parser.read('config.ini')
+# config.ini - enable of disable notifications
+notifications = parser.get('NOTIFY','enabled')
+## Add to config.ini
 #Server to check
-hostsfile = 'hosts.cfg'
+hostsfile = parser.get('GENERAL','hostsfile')
 #Downtime hostlist
 hostlist = {}
 notified = ['']
@@ -23,34 +26,37 @@ def hostLoad():
                 service = servicehost[0]
                 host = servicehost[1]
                 hostlist[host] = [service , False]
-            print(hostlist)
             logging.debug('Hosts: %s', hostlist)
     else:
         logging.error("Cannot find file specified, please try again.", exc_info=True)
+        exit(0)
 
 def notify(host, status, error):
-    mail_to = parser.get('NOTIFY','mail_to')
-    mail_from = parser.get('NOTIFY','mail_from')
-    mail_user = parser.get('NOTIFY','mail_user')
-    mail_pwd = parser.get('NOTIFY','mail_pwd')
-    mail_server = parser.get('NOTIFY','mail_server')
-    try:
-        smtpserver = smtplib.SMTP(mail_server,25)
-        smtpserver.ehlo()
-        #smtpserver.starttls()
-        smtpserver.ehlo
-        #smtpserver.login(mail_user,mail_pwd)
-        header = "To: {}\nFrom:{}\nSubject:{} {}\n".format(mail_to,mail_from,status,host)
-        input_message = error
-        message = header + input_message
-        smtpserver.sendmail(mail_user, mail_to, message)
-        smtpserver.close
-        print("Email sent to {}".format(mail_to))
-        notified.append(host)
-    except:
-        smtp_error = ("{} exception {}".format(datetime.datetime.now(),sys.exc_info()[0]))
-        logging.error(smtp_error)
-        print("Error sending mail: {}".format(smtp_error))
+    if notifications == True:
+        mail_to = parser.get('NOTIFY','mail_to')
+        mail_from = parser.get('NOTIFY','mail_from')
+        mail_user = parser.get('NOTIFY','mail_user')
+        mail_pwd = parser.get('NOTIFY','mail_pwd')
+        mail_server = parser.get('NOTIFY','mail_server')
+        try:
+            smtpserver = smtplib.SMTP(mail_server,25)
+            smtpserver.ehlo()
+            #smtpserver.starttls()
+            smtpserver.ehlo
+            #smtpserver.login(mail_user,mail_pwd)
+            header = "To: {}\nFrom:{}\nSubject:{} {}\n".format(mail_to,mail_from,status,host)
+            input_message = error
+            message = header + input_message
+            smtpserver.sendmail(mail_user, mail_to, message)
+            smtpserver.close
+            print("Email sent to {}".format(mail_to))
+            notified.append(host)
+        except:
+            smtp_error = ("{} exception {}".format(datetime.datetime.now(),sys.exc_info()[0]))
+            logging.error(smtp_error)
+            print("Error sending mail: {}".format(smtp_error))
+    else:
+        print('Notifications disabled')
 
 def hostCheck():
     while True:
@@ -59,6 +65,7 @@ def hostCheck():
             try:
                 check = requests.get(host)
                 hostcode = check.status_code
+                print(host,hostcode)
                 if hostcode == requests.codes.ok:
                     logging.info("{} {} returned status code {}".format(datetime.datetime.now(),host,hostcode))
                     hostlist[host][1] = 0
@@ -80,27 +87,28 @@ def hostCheck():
 def onError(host,error):
     hostlist[host][1] = 1
     print(error)
-    #if host not in notified:
-    #    notify(host, "DOWN!", error)
-    #else:
-    #    print("Notification already sent")
+    #start timer
+    if host not in notified:
+        notify(host, "DOWN!", error)
+    else:
+        print("Notification already sent")
 
-def hostError():
-    while True:
-        time.sleep(60)
-        for host,service,isdown in hostlist.items():
-            try:
-                check = requests.get(host)
-                hostcode = check.status_code
-                if hostcode == requests.codes.ok:
-                    error = ("{} {} Site back UP!".format(datetime.datetime.now(),host))
-                    logging.info(error)
-                    #hostlist[host] = False
-                    notify(host,"UP!",error)
-                else:
-                    print("{} Still DOWN!".format(host))
-            except:
-                print("{} Still DOWN!".format(host))
+#def hostError():
+#    while True:
+#        time.sleep(60)
+#        for host,service,isdown in hostlist.items():
+#            try:
+#                check = requests.get(host)
+#                hostcode = check.status_code
+#                if hostcode == requests.codes.ok:
+#                    error = ("{} {} Site back UP!".format(datetime.datetime.now(),host))
+#                    logging.info(error)
+#                    #hostlist[host] = False
+#                    notify(host,"UP!",error)
+#                else:
+#                    print("{} Still DOWN!".format(host))
+#            except:
+#                print("{} Still DOWN!".format(host))
 
 def writeHTML(hostlist):
     header = open("templates/header.html", "r")
